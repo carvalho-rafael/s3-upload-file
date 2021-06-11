c93ad4e4ec5b8ccf18b8c4960230c6c6be250fbf
Rafael Carvalho <rafaelvfcarvalho@gmail.com>
2021-06-10 21:55:41 -0300

    upload to disk with multer

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,24 @@ import express from 'express';
 import dotenv from 'dotenv'
 dotenv.config({ path: process.env.NODE_ENV === 'production' ? '.env.production' : '.env' })
 
+import multer from 'multer'
+import path from 'path'
+
 const app = express();
 
-app.use(express.json());
+app.use(
+  multer({
+    storage: multer.diskStorage({
+      destination: path.resolve(__dirname, '../', 'uploads/'),
+      filename: (req, file, cb) => {
+        cb(null, file.originalname)
+      }
+    })
+  }).single('file')
+)
 
-app.get('/upload', (req, res) => {
-  return res.json('hello')
-})
+app.post('/upload', (req, res, next) => {
+  res.send('File uploaded!');
+});
 
 app.listen(process.env.PORT || 3333);
